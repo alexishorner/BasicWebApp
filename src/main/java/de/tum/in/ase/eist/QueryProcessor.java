ebe62b9d7c54a91f0e945689388eeb58f52077e7
Alexis Horner <alexis.horner@tum.de>
2022-07-15 14:52:25 +0200

    Implement largest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,8 +2,12 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class QueryProcessor {
+
+    public static final String LARGEST = "which of the following numbers is the largest:";
 
     public String process(String query) {
 		query = query.toLowerCase();
@@ -13,7 +17,19 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        } else if (query.contains(LARGEST)) { // TODO extend the programm here
+            var numsString = query.substring(LARGEST.length());
+            var nums = numsString.split(",");
+            int[] numsInt = new int[nums.length];
+
+            for (int i = 0; i < nums.length; ++i) {
+                numsInt[i] = Integer.parseInt(nums[i]);
+            }
+
+            int max = Arrays.stream(numsInt).max().getAsInt();
+
+            return String.format("%d", max);
+        } else {
             return "";
         }
     }
