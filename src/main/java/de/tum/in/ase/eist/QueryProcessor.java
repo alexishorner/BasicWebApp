370afcf62b9bef9d541443431095c154930c7c06
Alexis Horner <alexis.horner@tum.de>
2022-07-15 15:23:43 +0200

    Fix plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -17,13 +17,30 @@ public class QueryProcessor {
            return "MyTeam";
         } else if (query.contains("largest")) { // TODO extend the programm here
             var numsString = query.substring(query.lastIndexOf(":") + 1)
-                                         .replace("%", "");
+                                  .replace("%", "");
             int max = Arrays.stream(numsString.split(","))
                             .mapToInt(Integer::parseInt)
                             .max()
                             .getAsInt();
 
             return String.format("%d", max);
+        } else if(query.contains("plus")) {
+            String is = "is";
+            int isEnd = query.lastIndexOf(is) + is.length();
+            String plus = "plus";
+            int plusBegin = query.lastIndexOf(plus);
+            int plusEnd = plusBegin + plus.length();
+            String sub = query.substring(isEnd).replaceFirst("%", "");
+            String aStr = sub.substring(0, sub.indexOf("%"))
+                               .replace("%", "");
+            String bStr = query.substring(plusEnd)
+                               .replace("%", "");
+
+            int a = Integer.parseInt(aStr);
+            int b = Integer.parseInt(bStr);
+            int sum = a + b;
+
+            return "%d".formatted(sum);
         } else {
             return "";
         }
--- src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
+++ src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
@@ -31,10 +31,19 @@ class QueryProcessorTest {
 	}
 
 	@Test
-	void numbers() {
+	void testLargestNumber() {
 		String ret = queryProcessor.process("%20which%20of%20the%20following%20numbers%20is%20the%20largest:%2079,%20675,%2024,%2033");
 		int actual = Integer.parseInt(ret);
 		assertEquals(20675, actual);
 	}
 
+	@Test
+	void testAddNumbers() {
+		int a = 2016;
+		int b = 200;
+		int expected = a + b;
+		String ret = queryProcessor.process("1a073e90:%20what%20is%" + a + "%20plus%" + b);
+		int actual = Integer.parseInt(ret);
+		assertEquals(expected, actual);
+	}
 }
